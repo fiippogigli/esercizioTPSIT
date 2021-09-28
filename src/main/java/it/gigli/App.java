5524d1ad0f7d253767165accba3a82bb69141c36
fiippogigli <76041375+fiippogigli@users.noreply.github.com>
2021-09-28 10:09:29 +0200

    estipsit

---
+++ src/main/java/it/gigli/App.java
@@ -0,0 +1,13 @@
+package it.gigli;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
